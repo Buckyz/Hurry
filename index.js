3d9db2341d72a196d9f4ecf2163e5c9319e1fc20
Bucky <118495316+Buckyz@users.noreply.github.com>
2022-12-20 22:49:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,47 @@
-var ticker = require('ticker')
-var game = require('./game-instance')
-
-require('raf')().on('data', function() {
-  game.tick()
-  game.draw()
-})
-
-function loop() {
-  // ticker(null, 60, 2)
-  //   .on('tick', function() { game.tick() })
-  //   .on('draw', function() { game.draw() })
-}
-
-process.nextTick(function() {
-  game.start()
-  loop()
-})
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>hurry!</title>
+    <style type="text/css">
+      body, html {
+        padding: 0;
+        margin: 0;
+        background: #222;
+      }
+      #highscore {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        color: #fff;
+        font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+        font-weight: 500;
+        font-size: 16px;
+        margin-top: 265px;
+        width: 400px;
+        margin-left: -200px;
+        text-align: center;
+      }
+      #main {
+        border: 1px dotted #fff;
+        -webkit-box-shadow: 0 0 30px -12px #fff;
+           -moz-box-shadow: 0 0 30px -12px #fff;
+            -ms-box-shadow: 0 0 30px -12px #fff;
+             -o-box-shadow: 0 0 30px -12px #fff;
+                box-shadow: 0 0 30px -12px #fff;
+        width: 800px;
+        height: 550px;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-left: -400px;
+        margin-top: -300px;
+      }
+    </style>
+</head>
+<body>
+    <div id="highscore">best score: <span id="score">0</span></div>
+    <canvas id="main"></canvas>
+    <script src="bundle.js" type="text/javascript"></script>
+</body>
+</html>
